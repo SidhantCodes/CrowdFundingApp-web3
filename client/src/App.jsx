2372ac29043d093837f552936b297105f8d9c204
Sidhant Mishra <mishrasidhant01@gmail.com>
2024-03-10 16:30:38 +0530

    Working on sidebar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
-
+import { Route, Routes } from 'react-router-dom'
+import { Navbar, Sidebar } from './components'
+import { CampaignDetails, CreateCampaign, Home, Profile } from './pages'
 const App = () => {
   return (
-    <div className='text-black bg-yellow-100 font-bold'>
-      App
+    <div className="relative sm:p-8 p-4 bg-[#13131a] min-h-screen flex flex-row">
+      <div className="sm:flex hidden mr-10 relative">
+        <Sidebar />
+      </div>
+      <div className="flex-1 max-sm:w-full max-w-[1280px] mx-auto sm:pr-5 ">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </div>
     </div>
   )
 }
